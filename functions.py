26c94d6166f98d5ce490271acd6ec85f421cf5e3
leschultz <laneenriqueschultz@gmail.com>
2019-06-12 09:14:09 -0500

    Able to create an input file with the template

---
+++ functions.py
@@ -0,0 +1,64 @@
+import random
+
+
+def run_creator(
+                template_contents,
+                elements,
+                fraction,
+                potential,
+                potential_type,
+                side,
+                unit_cell_type,
+                lattice_param,
+                timestep,
+                dump_rate,
+                ensemble,
+                holds
+                ):
+
+    # Random number used by LAMMPS
+    seed = random.randint(0, 9999999)
+
+    # Replace keywords within a template document
+    contents = template_contents
+    contents = contents.replace('#replace_elements#', elements)
+    contents = contents.replace('#replace_second_element_fraction#', fraction)
+    contents = contents.replace('#replace_potential#', potential)
+    contents = contents.replace('#replace_potential_type#', potential_type)
+    contents = contents.replace('#replace_side#', side)
+    contents = contents.replace('#replace_unit_cell_type#', unit_cell_type)
+    contents = contents.replace('#replace_lattice_param#', lattice_param)
+    contents = contents.replace('#replace_timestep#', timestep)
+    contents = contents.replace('#replace_dumprate#', dump_rate)
+    contents = contents.replace('#replace_ensemble#', ensemble)
+
+    # Randomize initial velocites
+    steps = (
+             'velocity all create ' +
+             str(holds[0][0]) +
+             ' ${seed} rot yes dist gaussian' +
+             2*'\n'
+             )
+
+    # Create a step for every temperature and hold defined
+    for temp, step in holds:
+        temp = str(temp)
+        step = str(step)
+        steps += (
+                  'fix step all npt temp ' +
+                  temp +
+                  ' ' +
+                  temp +
+                  ' '
+                  '0.1 ' +
+                  'iso 0 0 1\n' +
+                  'run ' +
+                  step +
+                  '\n' +
+                  'unfix step\n'
+                  )
+
+    contents = contents.replace('#replace_holds#', steps)
+    contents = contents.replace('#replace_seed#', str(seed))
+
+    return contents
---
+++ run-creator.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+from ast import literal_eval
+
+import sys
+
+from functions import *
+
+template = sys.argv[1]  # Template file path
+elements = sys.argv[2]  # Elements
+fraction = sys.argv[3]  # Fraction of second element
+potential = sys.argv[4]  # The potential used
+potential_type = sys.argv[5]  # The type of potential
+side = sys.argv[6]  # The length of the cubic simulation box
+unit_cell_type = sys.argv[7]  # fcc, hcp, or bcc
+lattice_param = sys.argv[8]  # The lattice paramter
+timestep = sys.argv[9]  # The timestep
+dump_rate = sys.argv[10]  # The rate to dump data
+ensemble = sys.argv[11]  # The ensemble for the holds
+holds = sys.argv[12:]  # temperature and holds as tuples
+
+# Open and read template
+template = open(template)
+template_contents = template.read()
+template.close()
+
+# Format the holds
+holds = list(map(literal_eval, holds))
+
+contents = run_creator(
+                       template_contents,
+                       elements,
+                       fraction,
+                       potential,
+                       potential_type,
+                       side,
+                       unit_cell_type,
+                       lattice_param,
+                       timestep,
+                       dump_rate,
+                       ensemble,
+                       holds
+                       )
+
+# Write the input file
+file_out = open('steps.in', 'w')
+file_out.write(contents)
+file_out.close()
+
+print(contents)
